e71e2d91d3c21c9dc6989c6e4c395333e220b8cd
Sushil <sushil.webexp@gmail.com>
2024-12-11 14:58:34 +0530

    check

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,28 +7,35 @@ const handler = NextAuth({
       name: "credentials",
       async authorize(credentials) {
         const { email, password, localData, stuData } = credentials;
-        const data = email === "test@gmail.com" && password === "123456zx"
-        if (data) {
-          console.log("email",email)
-          return { email, role: "admin" };
-        }
+       
         let parsedData = [];
         try {
           if (localData) parsedData = JSON.parse(localData);
           if (stuData) parsedData = [...JSON.parse(stuData), ...parsedData];
         } catch (error) {}
-        const user = parsedData.find(
-          (item) => item.email === email && item.password === password
-        );
-        if (user) {
-          return {
-            email: user.email,
-            password: user.password,
-            role: user.role,
-          };
-        } else {
-          return null;
+        try {
+          const user = parsedData.find(
+            (item) => item.email === email && item.password === password
+          );
+          const data = email === "test@gmail.com" && password === "123456zx"
+          if (data) {
+            console.log("email",email)
+            return { email, role: "admin" };
+          }
+          if (user||data) {
+            return {
+              email: user.email,
+              password: user.password,
+              role: user.role,
+            };
+          } else {
+            return null;
+          }
+        } catch (error) {
+          
         }
+       
+       
       },
     }),
   ],
